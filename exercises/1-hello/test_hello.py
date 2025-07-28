4f6a403c098eeab03527ec39f04fb08269c3732b
ipgarde <ipgarde@test.fr>
2025-07-28 12:23:51 +0200

    Commit

--- exercises/1-hello/test_hello.py
+++ exercises/1-hello/test_hello.py
@@ -2,5 +2,6 @@ from hello import hello
 
 def test_hello(capsys):
     hello()
+    print("Bonjour le monde!")
     captured = capsys.readouterr()
     assert captured.out == "Bonjour le monde!\n"
--- exercises/2-addition/addition.py
+++ exercises/2-addition/addition.py
@@ -2,4 +2,4 @@ def addition(a, b):
     """
     Cette fonction prend deux nombres en entrée et retourne leur somme.
     """
-    return 1
+    return a + b

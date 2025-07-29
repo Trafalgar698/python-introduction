d7d14674fa3c9820aa8bcbde733223b32000c1be
ipgarde <ipgarde@test.fr>
2025-07-29 15:28:49 +0200

    Commit

--- exercises/5-romanconverter/src/__init__.py
+++ exercises/5-romanconverter/src/__init__.py
@@ -1,1 +1,52 @@
 from .romanNumeral import *
+
+
+# roman_converter.py
+
+def int_to_roman(number: int) -> str:
+    res = ""
+    while number >= 10:
+        res += 'X'
+        number -= 10
+
+    if number == 9:
+        res += 'IX'
+        number -= 9
+
+    if number >= 4:
+        if number >= 5:
+            res += 'V'
+            number -= 5
+        else:
+            res += 'IV'
+            number -= 4
+
+    while number > 0:
+        res += 'I'
+        number -= 1
+
+    return res
+
+
+def roman_to_int(roman: str) -> int:
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman):
+        value = roman_map[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+            prev_value = value
+
+    return total
--- exercises/5-romanconverter/test_romanNumeral.py
+++ exercises/5-romanconverter/test_romanNumeral.py
@@ -1,7 +1,42 @@
 from src import *
-def test_roman_to_int():
-    assert roman_to_int (1) == 'I'
 
-def test_2_roman_to_int():
-    assert roman_to_int (2) == 'II'
+def test_int_to_roman_5():
+    # Maintenant le test est correct : 1 donne bien "I"
+    assert int_to_roman(1) == "I"
+    assert int_to_roman(2) == "II"
+    assert int_to_roman(3) == "III"
+    assert int_to_roman(4) == "IV"
+    assert int_to_roman(5) == "V"
+
+def test_int_to_roman_10():
+    assert int_to_roman(6) == "VI"
+    assert int_to_roman(7) == "VII"
+    assert int_to_roman(8) == "VIII"
+    assert int_to_roman(9) == "IX"
+
+def test_int_to_roman_20():
+    assert int_to_roman(10) == "X"
+    assert int_to_roman(20) == "XX"
+    assert int_to_roman(30) == "XXX"
+
+def test_roman_to_int_5():
+    assert roman_to_int("I") == 1
+    assert roman_to_int("II") == 2
+    assert roman_to_int("III") == 3
+    assert roman_to_int("IV") == 4
+    assert roman_to_int("V") == 5
+
+def test_roman_to_int10():
+    assert roman_to_int("VI") == 6
+    assert roman_to_int("VII") == 7
+    assert roman_to_int("VIII") == 8
+    assert roman_to_int("IX") == 9
+
+def test_roman_to_int11():
+    assert roman_to_int("X") == 10
+    assert roman_to_int("L") == 50
+    assert roman_to_int("C") == 100
+    assert roman_to_int("D") == 500
+
+
 

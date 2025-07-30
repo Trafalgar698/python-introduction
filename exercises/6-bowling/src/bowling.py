72d3b22657c16305dc9f5cf9cf6f479692d1a50c
ipgarde <ipgarde@test.fr>
2025-07-30 14:25:41 +0200

    dumb2 commit

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -3,11 +3,14 @@ def calcul_score(score: str) -> int :
     frames: list[str] = score.split()
     total = 0
     for frame in frames :
+        if '/' in frame :
+            total += 10
         for throw in frame :
             if frame == "-":
                 continue
 
             elif throw.isdigit():
                 total += int(throw)
+
         
     return total
--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -5,3 +5,7 @@ def test_shitty_player():
 
 def test_additional_score():
     assert calcul_score( "81 -- -- -- -- -- -- -- -- --") == 9
+    assert calcul_score("9- 8- -- -- -- -- -- -- -- --") == 17
+    assert calcul_score("5/ X -- -- -- -- -- -- -- --") == 5
+def test_spare():
+    assert calcul_score("5/ 8- -- -- -- -- -- -- -- --") == 18
